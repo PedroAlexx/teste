4a5daa1a851e38787d941a15aa84871bbf46a18d
Pedro Alexandre <104382913+PedroAlexx@users.noreply.github.com>
2022-05-11 17:23:13 -0300

    Update Exercicios_Aula4.java

--- exercicios_Aula4/src/main/java/com/mycompany/exercicios_aula4/Exercicios_Aula4.java
+++ exercicios_Aula4/src/main/java/com/mycompany/exercicios_aula4/Exercicios_Aula4.java
@@ -11,47 +11,84 @@ import java.util.Scanner;
  *
  * @author T-Gamer
  */
-class Aluno
-{
-    public String FirstName;  
-    public int Age; 
- };
+
 
 public class Exercicios_Aula4 {
+        public static void ordenaPessoasNome(String[] nomes, int[] idades, int quantidades){
+        int menorIndice;
+        for(int i = 0; i < quantidades; i++) {
+            
+            menorIndice = i;
+            for(int j = i + 1; j < quantidades; j++) {
+                if(nomes[menorIndice].compareTo(nomes[j]) > 0) {
+                    menorIndice = j;
+                }
+            }
+            
+            trocaPessoas(nomes, idades, quantidades, menorIndice, i);
+            
+        }
+        
+    }
+    public static void trocaPessoas(String[] nomes, int[] idades, int quantidades, int origem, int destino){
+        
+        String stringAux = nomes[origem];
+        nomes[origem] = nomes[destino];
+        nomes[destino] = stringAux;
+        
+        int intAux = idades[origem];
+        idades[origem] = idades[destino];
+        idades[destino] = intAux;
+        
+    }
+    public static void ImprimePessoas(String[] nomes, int[] idades, int quantidades){
+        Scanner teclado = new Scanner(System.in);
+        
+        for(int i = 0; i < quantidades; i++) {
+            System.out.println(nomes[i] + " == " + idades[i]);
+        }
+        
+        teclado.close();
+    }
+    public static void lePessoas(String[] args){
+        Scanner teclado = new Scanner(System.in);
+        
+        for(int i = 0; i < )
+    }
+    
+    public static void ordenaPessoasIdade(String[] nomes, int[] idades, int quantidades){
+        int menorIndice;
+        
+        for(int i = 0; i < quantidades; i++) {
+            
+            menorIndice = i;
+            for(int j = i + 1; j < quantidades; j++){
+                if(idades[menorIndice] > idades[j]){
+                    menorIndice = j;
+                }
+            } 
+        trocaPessoas(nomes, idades, quantidades, menorIndice, i);                            
+        }        
+    }
+    
 
     public static void main(String[] args) {
 
-        Scanner teclado = new Scanner(System.in);
+        final int QUANTIDADE = 5;
+        String[] nomes = new String[QUANTIDADE];
+        int[] idades = new int[QUANTIDADE];
+        
+        lePessoas(nomes, idades, QUANTIDADE);
+        
+        System.out.println("Ordenação por nome:");
+        ordenaPessoasNome(nomes, idades, QUANTIDADE);
+        ImprimePessoas(nomes, idades, QUANTIDADE);
 
-        String[] nome = new String[5];
-        int[] idade = new int[5];
-        for(int i = 0; i < 5; i++){
-            System.out.println("olá! insira o nome e a idade dos alunos");
-            Aluno estudante = new Aluno();
-            int idade_str = teclado.nextInt();
-            String nome_str = teclado.nextLine();
-            nome[i] = nome_str;
-            idade[i] = idade_str;            
-        }
-
-        System.out.println("Ordena por idade");
-            int menor = idade[0];
-           for(int i = 0; i < 5; i++)
-           {
-               
-               
-               if(idade[i] > idade[i+1]){
-                   menor = idade[i+1];
-                   idade[i] = menor;
-                   idade[i+1] = idade[i];
-               }
-           }
-            for(int i = 0; i < 5; i++)
-           {
-               System.out.println(idade[i]);
-           }
-           
+        System.out.println("Ordenação por idade:");
+        ordenaPessoasNome(nomes, idades, QUANTIDADE);
+        ImprimePessoas(nomes, idades, QUANTIDADE);
 
         
     }
+
 }
